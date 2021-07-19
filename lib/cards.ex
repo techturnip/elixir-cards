4eb248d26a8270c16ca8f526129ce8e0e38f8a37
Tyler Turnipseed <tyler@techturnip.us>
2021-07-19 17:01:38 -0400

    remove boilerplate

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,18 +1,2 @@
 defmodule Cards do
-  @moduledoc """
-  Documentation for `Cards`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Cards.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
